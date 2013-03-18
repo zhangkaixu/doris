7ed59faeb2b82561dc780cb8b7cdde3387e09a03
zhangkaixu <zhangkaixu@hotmail.com>
2013-03-18 20:49:31 +0800

    init sina

---
+++ doris/unigram.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import collections
+import argparse
+import sys
+
+
+def line_numbered(instream):
+    for ln,line in enumerate(instream):
+        if ln%100000==0 : 
+            print(ln,file=sys.stderr)
+        yield line
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--input',type=str, help='')
+    parser.add_argument('--output',type=str, help='')
+    args = parser.parse_args()
+
+    instream=open(args.input) if args.input else sys.stdin
+    outstream=open(args.output) if args.output else sys.stdout
+
+    counter=collections.Counter()
+
+    for line in line_numbered(instream) :
+        counter.update(line.strip())
+
+    for k,v in counter.most_common():
+        print(k,v,file=outstream)
+
--- doris/word/jump.py
+++ doris/word/jump.py
@@ -7,22 +7,25 @@ import sys
 
 if __name__ == '__main__':
     cs=set('#')
-    for line in open("unigram.txt"):
+    for line in open("data/high_freq.words"):
         c,v=line.split()
-        if int(v) < 100 : break
         cs.add(c)
 
 
     ln=0
     j=collections.Counter()
-    for line in open('cor.txt'):
+    for line in open('data/sina.news.txt'):
         ln+=1
-        if ln%1000==0 : print(ln,file=sys.stderr)
+        if ln%100000==0 : 
+            #12243297
+            #  100000
+            print(ln,file=sys.stderr)
+            #break
         line=line.strip()
         line='#'+line+'#'
-        s2=[(line[i],line[i+3])for i in range(len(line)-3)if line[i] in cs and line[i+3] in cs] 
-        s3=[(line[i],line[i+4])for i in range(len(line)-4)if line[i] in cs and line[i+4] in cs] 
-        s4=[(line[i],line[i+5])for i in range(len(line)-5)if line[i] in cs and line[i+5] in cs]
+        s2=[(line[i],line[i+3])for i in range(len(line)-3)if line[i+1:i+3] in cs] 
+        s3=[(line[i],line[i+4])for i in range(len(line)-4)if line[i+1:i+4] in cs] 
+        s4=[(line[i],line[i+5])for i in range(len(line)-5)if line[i+1:i+5] in cs] 
         j.update(s2)
         j.update(s3)
         j.update(s4)
